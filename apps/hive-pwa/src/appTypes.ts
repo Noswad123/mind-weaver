b2a8d1dbfdaf08afadffe62605a3b16b2cac4cfa
Jamal Dawson <Jamal.Dawson.@modernatx.com>
2026-07-10 15:13:53 -0400

    moved pwa app

--- apps/hive-pwa/src/appTypes.ts
+++
@@ -1,8 +0,0 @@
-export type StatusMessage = {
-  kind: 'idle' | 'ok' | 'error'
-  text: string
-}
-
-export type AppTab = 'notes' | 'todos' | 'sync' | 'settings'
-
-export type NoteMode = 'read' | 'edit'
